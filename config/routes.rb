3e17ae46b378fc56f8fa8aaa2c13ea1cc25f1fb1
elbincodes <elbincodes@gmail.com>
2017-09-24 17:20:21 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index]
+  resources :posts, only: [:index, :new, :create]
+
+  # I need to have a better understanding of exactly what is being said here?
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,5 +1,37 @@
+
 require 'rails_helper'
 
 describe 'new post' do
+  it 'ensures that the form route works with the /new action' do
+    visit new_post_path
+    expect(page.status_code).to eq(200)
+  end
+
+  it 'renders HTML in the /new template' do
+    visit new_post_path
+    expect(page).to have_content('Post Form')
+  end
+  # we got rid of the above because we dont want this to be a static page that just says "post form on it. but I just put it back in becuase I dont want to mess with any tests created.  "
+
+  it "displays a new post form that redirects to the index page, which then contains the submitted post's title and description" do
+      visit new_post_path
+      fill_in 'post_title', with: 'My post title'
+      fill_in 'post_description', with: 'My post description'
+
+      click_on 'Submit Post'
+
+      expect(page.current_path).to eq(posts_path)
+      expect(page).to have_content('My post title')
+      expect(page).to have_content('My post description')
+    end
 
 end
+
+
+
+
+# require 'rails_helper'
+#
+# describe 'new post' do
+#
+# end
